0299abae3b9b31554f78b3478406e8274db6b2ac
pspeegle <pspeegle@crimson.ua.edu>
2019-12-24 23:50:21 -0600

    added div switch in jQuery

--- src/script.js
+++ src/script.js
@@ -1,4 +1,53 @@
+var current_div = 1;
+var game_started = false;
+
+//this function is trash but if it aint broke (lamao.com)
+
+function parseKey(key_press){
+    if(key_press == 39 && current_div == 1){
+        current_div = 2;
+        $(".block1").css("opacity", 0.4);
+        $(".block2").css("opacity", 1.0);
+    }
+    if(key_press == 39 && current_div == 3){
+        current_div = 4;
+        $(".block3").css("opacity", 0.4);
+        $(".block4").css("opacity", 1.0);
+    }
+    if(key_press == 40 && current_div == 1){
+        current_div = 3;
+        $(".block1").css("opacity", 0.4);
+        $(".block3").css("opacity", 1.0);
+    }
+    if(key_press == 40 && current_div == 2){
+        current_div = 4;
+        $(".block2").css("opacity", 0.4);
+        $(".block4").css("opacity", 1.0);
+    }
+    if(key_press == 37 && current_div == 2){
+        current_div = 1;
+        $(".block2").css("opacity", 0.4);
+        $(".block1").css("opacity", 1.0);
+    }
+    if(key_press == 37 && current_div == 4){
+        current_div = 3;
+        $(".block4").css("opacity", 0.4);
+        $(".block3").css("opacity", 1.0);
+    }
+    if(key_press == 38 && current_div == 3){
+        current_div = 1;
+        $(".block3").css("opacity", 0.4);
+        $(".block1").css("opacity", 1.0);
+    }
+    if(key_press == 38 && current_div == 4){
+        current_div = 2;
+        $(".block4").css("opacity", 0.4);
+        $(".block2").css("opacity", 1.0);
+    }
+}
+
 function startGame() {
+    game_started = true;
     $(".start-container").hide();
     $(".problems-container").show();
 }
@@ -7,5 +56,8 @@ $(document).ready(function(){
     $(".problems-container").hide();
     $(".start-btn").click(function() {
         startGame();
+    });
+    $(document).keydown(function(event){
+        if(game_started === true) parseKey(event.which);
     });
 })
