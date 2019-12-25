ade898fb4310da99c2dc400d332aecc731457412
pspeegle <pspeegle@crimson.ua.edu>
2019-12-25 17:12:07 -0600

    Added user input on the bottom + some other things

--- src/script.js
+++ src/script.js
@@ -1,52 +1,72 @@
 var current_div = 1;
+var current_string = "";
 var game_started = false;
 
-//this function is trash but if it aint broke (lamao.com)
+/*literally looked everywhere on the internet
+ *could not find a better way to do this*/
 
 function parseKey(key_press){
     if(key_press == 39 && current_div == 1){
         current_div = 2;
         $(".block1").css("opacity", 0.4);
         $(".block2").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 39 && current_div == 3){
+    else if(key_press == 39 && current_div == 3){
         current_div = 4;
         $(".block3").css("opacity", 0.4);
         $(".block4").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 40 && current_div == 1){
+    else if(key_press == 40 && current_div == 1){
         current_div = 3;
         $(".block1").css("opacity", 0.4);
         $(".block3").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 40 && current_div == 2){
+    else if(key_press == 40 && current_div == 2){
         current_div = 4;
         $(".block2").css("opacity", 0.4);
         $(".block4").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 37 && current_div == 2){
+    else if(key_press == 37 && current_div == 2){
         current_div = 1;
         $(".block2").css("opacity", 0.4);
         $(".block1").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 37 && current_div == 4){
+    else if(key_press == 37 && current_div == 4){
         current_div = 3;
         $(".block4").css("opacity", 0.4);
         $(".block3").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 38 && current_div == 3){
+    else if(key_press == 38 && current_div == 3){
         current_div = 1;
         $(".block3").css("opacity", 0.4);
         $(".block1").css("opacity", 1.0);
+        current_string = "";
     }
-    if(key_press == 38 && current_div == 4){
+    else if(key_press == 38 && current_div == 4){
         current_div = 2;
         $(".block4").css("opacity", 0.4);
         $(".block2").css("opacity", 1.0);
+        current_string = "";
     }
+    else if(key_press >= 65 && key_press <= 90 || key_press >= 48 && key_press <= 57){
+        current_string += String.fromCharCode(key_press);
+    }
+    else if(key_press == 32){
+        current_string += " ";
+    }
+    else if(key_press == 8){
+        current_string = current_string.substring(0,current_string.length-1);
+    }
+    $(".text-input").html(current_string);
 }
 
-function startGame() {
+function startGame(){
     game_started = true;
     $(".start-container").hide();
     $(".problems-container").show();
@@ -54,7 +74,7 @@ function startGame() {
 
 $(document).ready(function(){
     $(".problems-container").hide();
-    $(".start-btn").click(function() {
+    $(".start-btn").click(function(){
         startGame();
     });
     $(document).keydown(function(event){
