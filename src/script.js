7b91f157706ed06772da46174fdb1d15c6a43a61
pspeegle <pspeegle@crimson.ua.edu>
2019-12-25 22:56:04 -0600

    Added support for ... LOWERCASE LETTERS!!! WOW!!

--- src/script.js
+++ src/script.js
@@ -1,13 +1,43 @@
 var current_div = 1;
 var current_string = "";
 var game_started = false;
+var current_problem4;
 
 var type_texts = [
-    "This is a test",
-    "Hello friends",
-    "qwertyuiop",
-    "Here is something really really long so that I can see what it does when it does not fit the screen",
-    "Quite indeed"
+    "This is a test of the intercom system.",
+    "Indeed.",
+    "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
+    "Quite indeed, considering the efficacy of Fermat's Little Theorem.",
+    "I encompass, and I eclipse. Ooh, got the hiccups.",
+    "Shawty come again; tell me what your mood 'bout; maybe we can move out; go to where it's nice out",
+    "Couldn't agree more, Stanley.",
+    "Why am I typing this?",
+    "I am doing this instead of my Ph.D. thesis",
+    "Ruby on Rails",
+    "Typing these all out was very fun.",
+    "A man, a plan, a canal. Panama.",
+    "Yea verily.",
+    "I been on the moon",
+    "She said I'm the man because I bought her Louboutins",
+    "1.e4 e5 2.Ke2!!",
+    "$we love jQuery :)",
+    "A module is Noetherian if and only if all of its submodules are finitely generated.",
+    "1.e4 e5 2.Nf3?? Ke7!!",
+    "Now I will do what others won't, so tomorrow I can do what others can't.",
+    "Cuius rei demonstrationem mirabilem sane detexi hanc marginis exiguitas non caperet. - Pierre de Fermat",
+    "Not much of a math game really is it",
+    "Koejawel.",
+    "Kweper.",
+    "The cycle of life and death continues: we will live; they will die.",
+    "The Bolzano-Weierstrass Theorem states that every bounded sequence in R^n has a convergent subsequence.",
+    "There exists a topology in which the real line is compact.",
+    "The rational numbers are a DENSE subset of the real numbers.",
+    "She said I'm the man because I look like Pierre Fermat",
+    "I came. I saw. I came. I saw. I praise - the lord. And break - the law.",
+    "YoooOOOOOoOOoOoooOooO",
+    "I really should've made a lorem ipsum dump instead of writing all these myself",
+    "Gordon Palmer",
+    "I love math!",
 ]
 
 var questions = [
@@ -93,9 +123,31 @@ function parseKey(key_press, shift_key){
         $(".input-message").css("color", "#666");
         current_string = "";
     }
-    //ascii code
-    else if(key_press >= 65 && key_press <= 90 || key_press >= 48 && key_press <= 57){
-        current_string += String.fromCharCode(key_press);
+    //ascii letters
+    else if(key_press >= 65 && key_press <= 90){
+        if(shift_key) current_string += String.fromCharCode(key_press);
+        else{
+            current_string += String.fromCharCode(key_press).toLowerCase();
+        }
+        $(".input-message").css("color", "#ddd");
+    }
+    //numbers and their respective shifts
+    else if(key_press >= 48 && key_press <= 57){
+        if(shift_key){
+            if(key_press == 48) current_string += ")";
+            if(key_press == 49) current_string += "!";
+            if(key_press == 50) current_string += "@";
+            if(key_press == 51) current_string += "#";
+            if(key_press == 52) current_string += "$";
+            if(key_press == 53) current_string += "%";
+            if(key_press == 54) current_string += "^";
+            if(key_press == 55) current_string += "&";
+            if(key_press == 56) current_string += "*";
+            if(key_press == 57) current_string += "(";
+        }
+        else{
+            current_string += String.fromCharCode(key_press);
+        }
         $(".input-message").css("color", "#ddd");
     }
     //comma
@@ -113,19 +165,30 @@ function parseKey(key_press, shift_key){
         current_string += ".";
         $(".input-message").css("color", "#ddd");
     }
-    //forward slash
+    //forward slash and question mark
     else if(key_press == 191){
-        current_string += "/";
+        if(!shift_key) current_string += "/";
+        else{
+            current_string += "?";
+        }
         $(".input-message").css("color", "#ddd");
     }
     //single quote
-    else if(key_press == 222 && shift_key == false){
+    else if(key_press == 222 && !shift_key){
         current_string += "'";
         $(".input-message").css("color", "#ddd");
     }
     //double quote
-    else if(key_press == 222 && shift_key == true){
+    else if(key_press == 222 && shift_key){
         current_string += "\"";
+        $(".input-message").css("color", "#ddd");
+    }
+    //semicolon and colon
+    else if(key_press == 186){
+        if(!shift_key) current_string += ";";
+        else{
+            current_string += ":";   
+        }
         $(".input-message").css("color", "#ddd");
     }
     //space
@@ -140,11 +203,26 @@ function parseKey(key_press, shift_key){
     }
     //enter (submit)
     else if(key_press == 13){
-        if($(".block3-problem").html().toUpperCase() == current_string.toUpperCase() && current_div == 3){
-            alert("This is true :)");
+        if(current_div == 3){
+            if($(".block3-problem").html() == current_string){
+                var index = Math.floor((Math.random() * type_texts.length));
+                var cs = type_texts[index];
+                $(".block3-problem").html(cs);
+            }
+            else{
+                alert("No sir (3).");
+            }
         }
-        else{
-            alert("This is false :(");
+        if(current_div == 4){
+            if(current_string.toUpperCase() == questions[current_problem4].answer.toUpperCase()){
+                var index = Math.floor((Math.random() * questions.length));
+                var cs = questions[index].question;
+                $(".block4-problem").html(cs);
+                current_problem4 = index;
+            }
+            else{
+                alert("No sir (4).");
+            }
         }
         current_string = "";
     }
@@ -155,9 +233,15 @@ function startGame(){
     game_started = true;
     $(".start-container").hide();
     $(".problems-container").show();
-    var index = Math.floor((Math.random() * type_texts.length));
-    var cs = type_texts[index];
-    $(".block3-problem").html(cs);
+
+    var index3 = Math.floor((Math.random() * type_texts.length));
+    var c3 = type_texts[index3];
+    $(".block3-problem").html(c3);
+
+    var index4 = Math.floor((Math.random() * questions.length));
+    var c4 = questions[index4].question;
+    $(".block4-problem").html(c4);
+    current_problem4 = index4;
 }
 
 $(document).ready(function(){
