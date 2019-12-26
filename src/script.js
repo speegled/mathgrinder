d4f91ced751e821a28a7e8aef801d23d63d7792b
pspeegle <pspeegle@crimson.ua.edu>
2019-12-25 21:26:09 -0600

    Added texts to type and many other things

--- src/script.js
+++ src/script.js
@@ -2,66 +2,117 @@ var current_div = 1;
 var current_string = "";
 var game_started = false;
 
+var type_texts = [
+    "This is a test", "Hello friends", "qwertyuiop", "Here is something really really long so that I can see what it does when it does not fit the screen"
+]
+
+var questions = [
+    {
+        "question": "4 x 5 = 20.",
+        "answer": "true"
+    }, 
+    {
+        "question": "Every commutative ring with one has a maximal ideal.",
+        "answer": "true"
+    },
+    {
+        "question": "The square root of 2 is algebraic.",
+        "answer": "true"
+    },
+    {
+        "question": "Pi is a rational number.",
+        "answer": "false"
+    }
+]
+
 /*literally looked everywhere on the internet
  *could not find a better way to do this*/
 
 function parseKey(key_press){
+    //right
     if(key_press == 39 && current_div == 1){
         current_div = 2;
         $(".block1").css("opacity", 0.4);
         $(".block2").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
     else if(key_press == 39 && current_div == 3){
         current_div = 4;
         $(".block3").css("opacity", 0.4);
         $(".block4").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
+    //down
     else if(key_press == 40 && current_div == 1){
         current_div = 3;
         $(".block1").css("opacity", 0.4);
         $(".block3").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
     else if(key_press == 40 && current_div == 2){
         current_div = 4;
         $(".block2").css("opacity", 0.4);
         $(".block4").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
+    //left
     else if(key_press == 37 && current_div == 2){
         current_div = 1;
         $(".block2").css("opacity", 0.4);
         $(".block1").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
     else if(key_press == 37 && current_div == 4){
         current_div = 3;
         $(".block4").css("opacity", 0.4);
         $(".block3").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
+    //up
     else if(key_press == 38 && current_div == 3){
         current_div = 1;
         $(".block3").css("opacity", 0.4);
         $(".block1").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
     else if(key_press == 38 && current_div == 4){
         current_div = 2;
         $(".block4").css("opacity", 0.4);
         $(".block2").css("opacity", 1.0);
+        $(".input-message").css("color", "#666");
         current_string = "";
     }
+    //ascii code
     else if(key_press >= 65 && key_press <= 90 || key_press >= 48 && key_press <= 57){
         current_string += String.fromCharCode(key_press);
+        $(".input-message").css("color", "#ddd");
     }
+    //space
     else if(key_press == 32){
         current_string += " ";
+        $(".input-message").css("color", "#ddd");
     }
+    //backspace
     else if(key_press == 8){
         current_string = current_string.substring(0,current_string.length-1);
+        $(".input-message").css("color", "#ddd");
+    }
+    //enter (submit)
+    else if(key_press == 13){
+        if($(".block3-problem").html().toUpperCase() == current_string.toUpperCase() && current_div == 3){
+            alert("This is true :)");
+        }
+        else{
+            alert("This is false :(");
+        }
+        current_string = "";
     }
     $(".text-input").html(current_string);
 }
@@ -70,6 +121,9 @@ function startGame(){
     game_started = true;
     $(".start-container").hide();
     $(".problems-container").show();
+    var index = Math.floor((Math.random() * type_texts.length));
+    var cs = type_texts[index];
+    $(".block3-problem").html(cs);
 }
 
 $(document).ready(function(){
