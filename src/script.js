24edc6ba1d756ad708ec8f6b20afeeb1ca2c7b04
pspeegle <pspeegle@crimson.ua.edu>
2019-12-26 21:32:05 -0600

    Working prototype ... it's pretty boring.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,33 @@
 var current_div = 1;
+var seconds_played = 0;
 var current_string = "";
 var game_started = false;
 var current_problem4;
+var current_operator1;
+var current_operator2;
+var f_1;
+var l_1;
+var f_2;
+var l_2;
+
+var possible_operators = ["+", "-", "x", "/"];
 
 var type_texts = [
+    "A penny saved is a penny earned.",
+    "Never look back. They might be gaining on you.",
+    "A stitch in time saves nine.",
+    "The only thing you have to fear is fear itself.",
+    "The rate at which inflation is increasing is decreasing.",
+    "However, the rate at which the increase of inflation is decreasing is increasing.",
+    "Rubber baby buggy bumpers.",
+    "All bubble blowing babies will be beaten senseless by every able-bodied patron in the bar (bar).",
+    "Every positive adjective was severely modified, if not completely contradicted by, an accompanying adverb.",
+    "Road work ahead? Uh, yeah. I sure hope it does!",
+    "East? I thought you said, \" Weast.\"",
+    "One must never contradict an active love",
+    "Are you calling me a liar? I ain't calling ya for dinner.",
+    "Hope you're happy.",
+    "Ride with the mob.",
     "Howdy.",
     "Pleased to meet you.",
     "Hello?",
@@ -12,12 +36,13 @@ var type_texts = [
     "Hello.",
     "Hi!",
     "Hey",
+    "Ferg is the name. Ben Baller did the chain.",
     "Today we have a Jelly Belly Pet Rat Gummi Candy.",
     "Affirmative",
     "Nobody knows, the troubles I've seen",
     "Nobody knows, my sorrow.",
     "5 plus 5 equals 10.",
-    "e5 is best by test.",
+    "e4 is best by test.",
     "This is a test of the intercom system.",
     "Indeed.",
     "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
@@ -61,6 +86,55 @@ var type_texts = [
     "Ok, times up!",
     "And I dust it off. A ha ha ha",
     "That angle",
+    "Walk out the crib with an ice cream sandwich.",
+    "Javaris Jamar Javarison-Lamar!",
+    "Davoin Shower-Handel",
+    "Jackmerius Tacktheritrix",
+    "Rome wasn't built in a day.",
+    "Listen. My motto is no beef: literal, or figurative.",
+    "I woke up in a new Bugatti",
+    "V A P O R W A V E",
+    "I want a brand new whip. I want a brand new house.",
+    "I fell in love - same time that you're falling apart",
+    "I shoulda known - thought I saw a sign in the stars",
+    "But I was wrong!",
+    "I used to be so devastated - at times I thought we'd never make it",
+    "My Ryze is bad. My Azir is worse. You guessed it right; I'm G2 Perkz.",
+    "Since LA, I been putting on",
+    "Cirque de Soleil, on and on and on",
+    "144 is a perfect square.",
+    "145 is not a perfect square.",
+    "Putting economic policy before fiscal responsibility is like putting the cart before the horse.",
+    "Venmo me your lunch money, nerd.",
+    "Need to get the lambo and drive",
+    "I was in the lab; you were on my mind. But all I really do is just grind",
+    "Who'd do the stuff that I do??",
+    "I coulda went (sic) to school to be a doctor. But I dropped out and chose to be a baller.",
+    "Cash in the area; what goes up, gotta come down.",
+    "I'm so glad that you came",
+    "I'm trapped in my conscious",
+    "Where's the love if you love me?",
+    "The sky keep on falling",
+    "I don't wanna buy no more",
+    "Ima die rich. Keep a side chick. That's just how it is - just in case you didn't know",
+    "Represent the men who get reckless for that yen",
+    "I'll never let you let me go",
+    "Yo Pierre, you wanna come out here?",
+    "In New York I Milly Rock",
+    "Baking soda! I got baking soda!",
+    "We dem boyz",
+    "I got loyalty, got royalty inside my DNA",
+    "I got millions, I got riches building in my DNA",
+    "Every time I'm in the street, I hear, \"Yok Yok Yok Yok\".",
+    "Yeah, something inside me's changed - I was so much younger yesterday",
+    "Swish! I'm balling. I'm so awesome.",
+    "They said I wouldn't be nothing; now they always say, \"Congratulations!\"",
+    "Oh, I want something just like this!",
+    "I was about to text you back... but then I hopped up in the drop",
+    "Walking with a check, I take lil' shawty out for lunch",
+    "Walk. We don't no talking. Got your man calling. I need better options.",
+    "I came, I saw. I conquered all.",
+    "Pull up, shoot! Jayson Tatum",
     "I went on a date with this girl, and her floor was just bean bags.",
     "Soulja boy up in it; watch me crank it, watch me; watch me crank that soulja boy",
     "\"I dunno. CAN you go to the bathroom? Can you?\" - My middle school teacher, Mrs. Trisket",
@@ -79,7 +153,6 @@ var type_texts = [
     "\"Dreaming, after all, is a form of planning.\" - Gloria Steinem",
     "\"I have no special talent. I am only passionately curious.\" - Albert Einstein",
     "\"Wisely, and slow. They stumble that run fast.\" - William Shakespeare",
-
 ]
 
 var questions = [
@@ -90,6 +163,11 @@ var questions = [
     }, 
     {
         "question": "Every commutative ring with one has a maximal ideal.",
+        "answer": "true",
+        "difficulty": "hard"
+    },
+    {
+        "question": "All maximal ideals are prime ideals.",
         "answer": "true",
         "difficulty": "hard"
     },
@@ -144,14 +222,9 @@ var questions = [
         "difficulty": "easy"
     },
     {
-        "question": "If the sum of two prime numbers is even, then one of them has to be 2.",
+        "question": "If the sum of two prime numbers is odd, then one of them has to be 2.",
         "answer": "true",
         "difficulty": "easy"
-    },
-    {
-        "question": "In the ring of even integers (2Z), 42 is a prime number.",
-        "answer": "true",
-        "difficulty": "hard"
     },
     {
         "question": "The sides of a triangle can be uniquely determined by the measure of its angles.",
@@ -424,25 +497,106 @@ function parseKey(key_press, shift_key){
     }
     //enter (submit)
     else if(key_press == 13){
-        if(current_div == 3){
+        if(current_div == 1){
+            var correct_ans = false;
+            if(current_operator1 == "+" && current_string == f_1 + l_1){
+                correct_ans = true;
+            }
+            else if(current_operator1 == "-" && current_string == f_1 - l_1){
+                correct_ans = true;
+            }
+            else if(current_operator1 == "x" && current_string == f_1 * l_1){
+                correct_ans = true;
+            }
+            else if(current_operator1 == "/" && current_string == f_1 / l_1){
+                correct_ans = true;
+            }
+
+            if(correct_ans){
+                var index1 = Math.floor((Math.random() * 4));
+                current_operator1 = possible_operators[index1];
+                l_1 = Math.floor((Math.random() * (Math.floor(5 + seconds_played/10)))) + 1;
+                if(current_operator1 == "/"){
+                    f_1 = Math.floor((Math.random()* (Math.floor(5 + seconds_played/10))))*l_1;
+                }
+                else{
+                    f_1 = Math.floor((Math.random() * (Math.floor(5 + seconds_played/10))));
+                }
+                var c1 = f_1.toString() + " " + current_operator1 + " " + l_1.toString();
+                $(".block1-problem").html(c1);
+
+                var t1 = parseInt($(".timer1").html());
+                t1+=8;
+                t1.toString();
+                $(".timer1").html(t1);
+            }
+        }
+        else if(current_div == 2){
+            var correct_ans = false;
+            if(current_operator2 == "+" && current_string == f_2 + l_2){
+                correct_ans = true;
+            }
+            else if(current_operator2 == "-" && current_string == f_2 - l_2){
+                correct_ans = true;
+            }
+            else if(current_operator2 == "x" && current_string == f_2 * l_2){
+                correct_ans = true;
+            }
+            else if(current_operator2 == "/" && current_string == f_2 / l_2){
+                correct_ans = true;
+            }
+
+            if(correct_ans){
+                var index2 = Math.floor((Math.random() * 4));
+                current_operator2 = possible_operators[index2];
+                l_2 = Math.floor((Math.random() * (Math.floor(7 + seconds_played/8)))) + 1;
+                if(current_operator2 == "/"){
+                    f_2 = Math.floor((Math.random()* (Math.floor(7 + seconds_played/8))))*l_2;
+                }
+                else{
+                    f_2 = Math.floor((Math.random() * (Math.floor(5 + seconds_played/10))));
+                }
+                var c2 = f_2.toString() + " " + current_operator2 + " " + l_2.toString();
+                $(".block2-problem").html(c2);
+
+                var t2 = parseInt($(".timer2").html());
+                t2+=15;
+                $(".timer2").html(t2);
+            }
+        }
+        else if(current_div == 3){
             if($(".block3-problem").html() == current_string){
+                var t3 = parseInt($(".timer3").html());
+                t3+= (12 + Math.floor(current_string.length/15));
+                $(".timer3").html(t3);
+
                 var index = Math.floor((Math.random() * type_texts.length));
                 var cs = type_texts[index];
                 $(".block3-problem").html(cs);
             }
             else{
-                alert("No sir (3).");
+                //make something red -- alert user somehow that he messed up
             }
         }
-        if(current_div == 4){
+        else if(current_div == 4){
             if(current_string.toUpperCase() == questions[current_problem4].answer.toUpperCase()){
                 var index = Math.floor((Math.random() * questions.length));
                 var cs = questions[index].question;
                 $(".block4-problem").html(cs);
                 current_problem4 = index;
+
+                var t4 = parseInt($(".timer4").html());
+                t4 += 6;
+                $(".timer4").html(t4);
             }
             else{
-                alert("No sir (4).");
+                //make something red -- alert user somehow that he messed up
+                var t4 = parseInt($(".timer4").html());
+                if(t4 < 8) t4 = 1;
+                else{
+                    t4 -= 6;
+                }
+                $(".timer4").html(t4);
             }
         }
         current_string = "";
@@ -455,6 +609,30 @@ function startGame(){
     $(".start-container").hide();
     $(".problems-container").show();
 
+    var index1 = Math.floor((Math.random() * 4));
+    current_operator1 = possible_operators[index1];
+    l_1 = Math.floor((Math.random() * (Math.floor(5 + seconds_played/10)))) + 1;
+    if(current_operator1 == "/"){
+        f_1 = Math.floor((Math.random()* (Math.floor(5 + seconds_played/10))))*l_1;
+    }
+    else{
+        f_1 = Math.floor((Math.random() * (Math.floor(5 + seconds_played/10))));
+    }
+    var c1 = f_1.toString() + " " + current_operator1 + " " + l_1.toString();
+    $(".block1-problem").html(c1);
+
+    var index2 = Math.floor((Math.random() * 4));
+    current_operator2 = possible_operators[index2];
+    l_2 = Math.floor((Math.random() * (Math.floor(7 + seconds_played/8)))) + 1;
+    if(current_operator2 == "/"){
+        f_2 = Math.floor((Math.random()* (Math.floor(7 + seconds_played/8))))*l_2;
+    }
+    else{
+        f_2 = Math.floor((Math.random() * (Math.floor(5 + seconds_played/10))));
+    }
+    var c2 = f_2.toString() + " " + current_operator2 + " " + l_2.toString();
+    $(".block2-problem").html(c2);
+
     var index3 = Math.floor((Math.random() * type_texts.length));
     var c3 = type_texts[index3];
     $(".block3-problem").html(c3);
@@ -463,6 +641,42 @@ function startGame(){
     var c4 = questions[index4].question;
     $(".block4-problem").html(c4);
     current_problem4 = index4;
+    count_time();
+}
+
+function tick(){
+    var t1 = parseInt($(".timer1").html());
+    t1--;
+    t1.toString();
+    $(".timer1").html(t1);
+    
+    var t2 = parseInt($(".timer2").html());
+    t2--;
+    t2.toString();
+    $(".timer2").html(t2);
+
+    var t3 = parseInt($(".timer3").html());
+    t3--;
+    t3.toString();
+    $(".timer3").html(t3);
+
+    var t4 = parseInt($(".timer4").html());
+    t4--;
+    t4.toString();
+    $(".timer4").html(t4);
+
+    if(t1 <= 0 || t2 <= 0 || t3 <= 0 || t4 <= 0){
+        clearInterval(t);
+        alert("You lose.");
+        $(".problems-container").hide();
+        $(".start-container").hide();
+        game_started = false;
+    }
+    seconds_played++;
+}
+
+function count_time(){
+    t = setInterval(tick, 1000);
 }
 
 $(document).ready(function(){
