ea685e5d921e2b9a88b205de8bd8db53e7cca712
pspeegle <pspeegle@crimson.ua.edu>
2019-12-26 23:06:04 -0600

    favicon stuff added

---
+++ src/questions.js
@@ -0,0 +1,197 @@
+var questions = [
+    {
+        "question": "4 x 5 = 20.",
+        "answer": "true",
+        "difficulty": "easy"
+    }, 
+    {
+        "question": "Every commutative ring with one has a maximal ideal.",
+        "answer": "true",
+        "difficulty": "hard"
+    },
+    {
+        "question": "All maximal ideals are prime ideals.",
+        "answer": "true",
+        "difficulty": "hard"
+    },
+    {
+        "question": "The square root of 2 is algebraic.",
+        "answer": "true",
+        "difficulty": "medium"
+    },
+    {
+        "question": "Pi is a rational number.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "The first few digits of pi are 3.141592.",
+        "answer": "true",
+        "difficulty": "medium"
+    },
+    {
+        "question": "The first few digits of pi are 3.141593.",
+        "answer": "false",
+        "difficulty": "medium"
+    },
+    {
+        "question": "The first few digits of pi are 3.14159261.",
+        "answer": "false",
+        "difficulty": "medium"
+    },
+    {
+        "question": "Pi has finitely many digits.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "16 x 11 = 186.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "17 x 7 = 119",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "14 - 42 = -32",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "The sum of two prime numbers is even.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "If the sum of two prime numbers is odd, then one of them has to be 2.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "The sides of a triangle can be uniquely determined by the measure of its angles.",
+        "answer": "false",
+        "difficulty": "medium"
+    },
+    {
+        "question": "(7 * 7) mod 4 is equivalent to 1 mod 4.",
+        "answer": "true",
+        "difficulty": "medium"
+    },
+    {
+        "question": "52 + 298 = 340.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "91 is prime.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "133 is prime.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "171 is prime.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "97 is prime.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "101 is prime.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "1 is prime.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "0 is prime.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "12 x 24 = 268.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "361 is a square number.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "The binary representation for 17 (base 10) is 10101.",
+        "answer": "false",
+        "difficulty": "medium"
+    },
+    {
+        "question": "The derivative of sin(2x) is 2sinxcosx.",
+        "answer": "false",
+        "difficulty": "medium"
+    },
+    {
+        "question": "If a function is continuous, it is differentiable.",
+        "answer": "false",
+        "difficulty": "medium"
+    },
+    {
+        "question": "3 x 4 = 12.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "107 + 789 = 896.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "4096 is a power of 2.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "There are 3 prime numbers between 8 and 16.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "186 is divisible by 7.",
+        "answer": "false",
+        "difficulty": "easy"
+    },
+    {
+        "question": "279 is divisible by 3.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "3, 5, and 7 are the only set of 3 consecutive primes that are 2 apart.",
+        "answer": "true",
+        "difficulty": "easy"
+    },
+    {
+        "question": "The definite integral from 1 to 5 of 3x^2 dx is 126.",
+        "answer": "false",
+        "difficulty": "hard"
+    },
+    {
+        "question": "The definite integral from 1 to 5 of 3x^2 dx is 124.",
+        "answer": "true",
+        "difficulty": "hard"
+    },
+    {
+        "question": "The definite integral from 2 to 5 of 3x^2 dx is 121.",
+        "answer": "true",
+        "difficulty": "hard"
+    },
+]
